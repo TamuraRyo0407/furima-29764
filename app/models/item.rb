2bb55334daa82d0e319e5f9adfc4e8c5beda8635
TamuraRyo0407 <asacafe06@gmail.com>
2020-12-11 19:20:13 +0900

    レビュー修正1回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to       :user
-  has_one          :buyer
+# has_one          :buyer
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -12,12 +12,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text 
-    validates :category_id, numericality: { other_than: 1 }  
-    validates :condition_id, numericality: { other_than: 1 } 
-    validates :delivery_fee_defrayer_id, numericality: { other_than: 1 }  
-    validates :forwarder_area_id, numericality: { other_than: 1 } 
-    validates :sending_day_id, numericality: { other_than: 1 } 
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :image
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
+  with_options presence: true , numericality: { other_than: 1 }  do
+  validates :category_id
+  validates :condition_id
+  validates :delivery_fee_defrayer_id
+  validates :forwarder_area_id
+  validates :sending_day_id
   end
 end
